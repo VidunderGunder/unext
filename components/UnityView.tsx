26df000e7ec80c8f2bd5eeed368ad3dea8519b78
VidunderGunder <kristiankramas@outlook.com>
2021-02-11 21:42:39 +0100

    Unity fills canvas

--- components/UnityView.tsx
+++ components/UnityView.tsx
@@ -1,6 +1,9 @@
 import Unity, { UnityContext } from "react-unity-webgl";
+import { use100vh } from "react-div-100vh";
 
 export default function UnityView() {
+  const height = use100vh();
+
   const unityContext = new UnityContext({
     loaderUrl: "build/build.loader.js",
     dataUrl: "build/build.data",
@@ -8,5 +11,9 @@ export default function UnityView() {
     codeUrl: "build/build.wasm",
   });
 
-  return <Unity unityContext={unityContext} />;
+  return (
+    <div style={{ height }}>
+      <Unity unityContext={unityContext} width="100%" height="100%" />
+    </div>
+  );
 }
