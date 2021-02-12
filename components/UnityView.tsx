8733ae3f3d710e9a6878986a0444ed05ff3b97e5
VidunderGunder <kristiankramas@outlook.com>
2021-02-12 15:29:28 +0100

    Fullscreen attempt

--- components/UnityView.tsx
+++ components/UnityView.tsx
@@ -4,6 +4,10 @@ import { use100vh } from "react-div-100vh";
 export default function UnityView() {
   const height = use100vh();
 
+  if (!process.browser && !height) {
+    return null;
+  }
+
   const unityContext = new UnityContext({
     loaderUrl: "build/build.loader.js",
     dataUrl: "build/build.data",
@@ -13,22 +17,20 @@ export default function UnityView() {
 
   return (
     <>
-      {process.browser && height && (
-        <div style={{ height, width: "100%" }}>
-          <Unity
-            unityContext={unityContext}
-            width="100%"
-            height="100%"
-            className="unity"
-          />
-          <style jsx>{`
-            .unity {
-              height: 100%;
-              width: 100%;
-            }
-          `}</style>
-        </div>
-      )}
+      <div style={{ height, width: "100%" }}>
+        <Unity
+          unityContext={unityContext}
+          width="100%"
+          height="100%"
+          className="unity"
+        />
+        <style jsx>{`
+          .unity {
+            height: 100%;
+            width: 100%;
+          }
+        `}</style>
+      </div>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import dynamic from "next/dynamic";
+import UnityView from "components/UnityView";
 
-const UnityView = dynamic(() => import("../components/UnityView"), {
-  ssr: false,
-});
+// const UnityView = dynamic(() => import("../components/UnityView"), {
+//   ssr: false,
+// });
 
 export default function Home() {
   return <UnityView />;
