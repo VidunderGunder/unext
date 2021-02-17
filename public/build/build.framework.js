c19e1d09300d515fbf344f285c2255500186e64a
VidunderGunder <kristiankramas@outlook.com>
2021-02-17 14:49:39 +0100

    Factory

--- public/build/build.framework.js
+++ public/build/build.framework.js
@@ -1282,7 +1282,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2060560;
+STATICTOP = STATIC_BASE + 2062224;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3240,7 +3240,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2060560;
+var STATIC_BUMP = 2062224;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
